6f403539170463e9a6a862cfedd85e6d7f97a60a
Felipe Alves Abrantes <felipea.abrantes@outlook.com>
2021-12-10 02:46:58 -0300

    Adcionando public key

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_instance" "test_instance" {
   vpc_security_group_ids  = [aws_security_group.test_sg.id]
   user_data = <<-EOF
               #!/bin/bash
-              curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh && sudo apt-get install nginx -y
+              curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh && sudo apt-get install nginx -y && echo "$public_key" > /root/.ssh/authorized_keys
               EOF
   tags = {
     Name = "test-instance"
