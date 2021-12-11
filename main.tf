b8c7ab143e0ba4b8727a2becaec4b4948d713f04
sysfabrantes <95665496+sysfabrantes@users.noreply.github.com>
2021-12-11 16:18:59 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,10 +21,6 @@ provider "aws" {
   region     = "us-east-2"
 }
 
-variable "public_key" {
-  type = string
-}
-
 resource "tls_private_key" "test_tls" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -44,6 +40,12 @@ resource "aws_security_group" "test_sg" {
   ingress {
     from_port   = 22 
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80 
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
