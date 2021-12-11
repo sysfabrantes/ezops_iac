2c8d814c6bc50c4cec2095ece6e28ee4daee7c24
sysfabrantes <95665496+sysfabrantes@users.noreply.github.com>
2021-12-11 16:33:21 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,8 +48,8 @@ resource "aws_security_group" "test_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 80 
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
