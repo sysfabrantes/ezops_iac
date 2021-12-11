7e3bcc0855033ac4f9c093d9208dfedd2b863b78
sysfabrantes <95665496+sysfabrantes@users.noreply.github.com>
2021-12-11 16:47:42 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,23 +21,23 @@ provider "aws" {
   region     = "us-east-2"
 }
 
-variable "public_key" {
-  type        = string
-}
+# variable "public_key" {
+#   type        = string
+# }
 
-resource "tls_private_key" "test_tls" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+# resource "tls_private_key" "test_tls" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
 
-resource "aws_key_pair" "test_key" {
-  key_name   = "test-key"
-  public_key = tls_private_key.test_tls.public_key_openssh
+# resource "aws_key_pair" "test_key" {
+#   key_name   = "test-key"
+#   public_key = tls_private_key.test_tls.public_key_openssh
 
-  provisioner "local-exec" { 
-    command = "echo '${tls_private_key.test_tls.private_key_pem}' > ./test-key.pem"
-  }
-}
+#   provisioner "local-exec" { 
+#     command = "echo '${tls_private_key.test_tls.private_key_pem}' > ./test-key.pem"
+#   }
+# }
 
 resource "aws_security_group" "test_sg" {
   name = "test-sg"
@@ -58,7 +58,7 @@ resource "aws_security_group" "test_sg" {
 resource "aws_instance" "test_instance" {
   ami           = "ami-0629230e074c580f2"
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.test_key.key_name
+  key_name      =  "deploy-nginx-key" #aws_key_pair.test_key.key_name
   vpc_security_group_ids  = [aws_security_group.test_sg.id]
   user_data = <<-EOF
               #!/bin/bash
